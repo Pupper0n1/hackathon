8954a993b2056d59b6a7f3e8c2e58e826a9be330
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-02-18 14:11:39 -0700

    Added login, logout, signup, and installe
    Installed bootstrap 5 styelsheet and scripts via CDN
    
    Added login, logout, signup paths and finished up the forms

--- dinobuddies/dinobuddies/urls.py
+++ dinobuddies/dinobuddies/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
 ]
