b4260df5fc849d90ee4ccbdc58f6c01e63be7aa5
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-02-18 14:31:53 -0700

    Fixed bugs in signup.html and base.html

--- dinobuddies/accounts/urls.py
+++ dinobuddies/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
-from django.contrib.auth import views as authentication_views
+from django.contrib.auth import views as authenticator_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', authentication_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
-    path('logout/', authentication_views.LogoutView.as_view(), name="logout"),
+    path('login/', authenticator_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('logout/', authenticator_views.LogoutView.as_view(), name="logout"),
     path('signup', views.SignUp.as_view(), name='signup')
 ]
