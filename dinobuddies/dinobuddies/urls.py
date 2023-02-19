74674a60ab39744093ba74da71e420dbc9c41197
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-02-18 19:37:47 -0700

    Linked pages

--- dinobuddies/dinobuddies/urls.py
+++ dinobuddies/dinobuddies/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
-    path('gallery/', views.GalleryPage.as_view(), name='gallery')
+    path('gallery/', views.GalleryPage.as_view(), name='gallery'),
+    path('goodbye/', views.GalleryPage.as_view(), name='goodbye')
 ]
